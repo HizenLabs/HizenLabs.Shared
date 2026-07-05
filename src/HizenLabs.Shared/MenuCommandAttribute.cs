ad305ba88d6a335d239ed60acba396e4b58c2358
Ethan Delong <48994953+EthanDelong@users.noreply.github.com>
2026-07-05 18:50:47 -0400

    add menucommand attribute alias

---
+++ src/HizenLabs.Shared/MenuCommandAttribute.cs
@@ -0,0 +1,11 @@
+﻿#if CARBON
+public class MenuCommandAttribute : ProtectedCommandAttribute
+{
+    public MenuCommandAttribute(string name) : base(name) { }
+}
+#else
+public class MenuCommandAttribute : ConsoleCommandAttribute
+{
+    public MenuCommandAttribute(string command) : base(command) { }
+}
+#endif
